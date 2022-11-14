83b6ac14667fe6b67caf6d0456984d3321bdcc41
TheWisePigeon <zozozozeph@gmail.com>
2022-11-14 21:11:21 +0000

    feat: added landing screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class=" flex flex-col h-screen items-center justify-center" >
+    <h1 class=" text-6xl md:text-7xl lg:text-9xl underline hor " >
+        Who Are You 
+    </h1>
+    <div class="hor animate-bounce cursor-pointer flex justify-center text-white w-20 h-10 mt-10  ">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3 8.688c0-.864.933-1.405 1.683-.977l7.108 4.062a1.125 1.125 0 010 1.953l-7.108 4.062A1.125 1.125 0 013 16.81V8.688zM12.75 8.688c0-.864.933-1.405 1.683-.977l7.108 4.062a1.125 1.125 0 010 1.953l-7.108 4.062a1.125 1.125 0 01-1.683-.977V8.688z" />
+        </svg>          
+    </div>
+</div>
